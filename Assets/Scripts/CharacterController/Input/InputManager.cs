eb90b4976df90c607b831650ec4e3c49f1f323f8
Paulo Massa <paulogabrielmassa@gmail.com>
2023-05-24 17:22:04 -0300

    Created the InputManager class

---
+++ Assets/Scripts/CharacterController/Input/InputManager.cs
@@ -0,0 +1,37 @@
+/*
+ * This class is responsible for enabling and disabling the InputSystem, as well as
+ * changing ActionMaps(not yet implemented). It also receives and stores the inputs of each Action.
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerInputs playerInputs;
+
+    // All the input values must have a public Get and a private Set
+    public Vector2 directionalInput { get; private set; } //WASD input
+
+    private void OnEnable()
+    {
+        // Initializes playerInputs and set the apropriate callback methods to handle each Action
+        if (playerInputs == null)
+        {
+            playerInputs = new PlayerInputs();
+
+            playerInputs.Movement.DirectionalMovement.performed += i => directionalInput = i.ReadValue<Vector2>();
+        }
+
+        playerInputs.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInputs.Disable();
+    }
+
+}
