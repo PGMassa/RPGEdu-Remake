65b65c8c6063c76f63512574c9ea023e4bff3253
Paulo Massa <paulogabrielmassa@gmail.com>
2023-06-02 23:01:43 -0300

    bekin rotates to face the player

---
+++ Assets/Scripts/NPCs/RotationToTarget.cs
@@ -0,0 +1,36 @@
+/*
+ * This class only rotates the npc in the direction of the player
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationToTarget : MonoBehaviour
+{
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private float rotationSpeed;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        Vector3 targetDirection = targetTransform.position - transform.position;
+        targetDirection.y = 0;
+
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+        targetRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        rb.MoveRotation(targetRotation);
+    }
+}
